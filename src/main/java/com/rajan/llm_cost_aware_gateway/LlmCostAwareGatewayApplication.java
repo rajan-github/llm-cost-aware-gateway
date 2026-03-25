fabce36d8367f281bca2ffbb6dceb2dccdc63ba7
rajan chauhan <rajanchauhan@rajans-MacBook-Pro.local>
2026-03-25 21:21:59 +0530

    Added initial spring-boot config and setup the project

---
+++ src/main/java/com/rajan/llm_cost_aware_gateway/LlmCostAwareGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.rajan.llm_cost_aware_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LlmCostAwareGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LlmCostAwareGatewayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/rajan/llm_cost_aware_gateway/LlmCostAwareGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rajan.llm_cost_aware_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LlmCostAwareGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
